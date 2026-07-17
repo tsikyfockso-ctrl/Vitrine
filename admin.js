0992f090e91cfa392c0ad0abf57d6bb699d15ce5
FreeDomain <tsikyfockso@gmail.com>
2026-07-17 17:22:27 +0300

    Add logout functionality for admin user

---
+++ admin.js
@@ -0,0 +1,7 @@
+document.getElementById('logoutBtn').addEventListener('click', function() {
+    // Supprime la clé qui indique que l'admin est connecté
+    localStorage.removeItem("isAdmin");
+    
+    // Redirige l'utilisateur vers la page de login
+    window.location.href = "login.html";
+});
