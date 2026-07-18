c4f1caf673da243bde1e9c2ce39c92ea62ec975a
FreeDomain <tsikyfockso@gmail.com>
2026-07-18 14:02:36 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -5,26 +5,3 @@ document.getElementById('logoutBtn').addEventListener('click', function() {
     // Redirige l'utilisateur vers la page de login
     window.location.href ="login.html";
 });
-function showNotification(message, type = 'success') {
-    const container = document.getElementById('notification-container');
-    const note = document.createElement('div');
-    note.className = `notification ${type}`;
-    note.innerText = message;
-    
-    container.appendChild(note);
-    
-    // Supprime la notification après 3 secondes
-    setTimeout(() => {
-        note.remove();
-    }, 3000);
-}
-// Exemple : Notification lors de la déconnexion
-document.getElementById('logoutBtn').addEventListener('click', function() {
-    showNotification("Déconnexion réussie...", "success");
-    
-    // Attendre un peu avant de rediriger pour que l'admin voie le message
-    setTimeout(() => {
-        localStorage.removeItem("isAdmin");
-        window.location.href = "login.html";
-    }, 1000);
-});
