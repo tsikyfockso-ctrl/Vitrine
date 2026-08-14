85852b3643c6b9778572c249e29b2c44a7e93e74
Network Access <tsikyfockso@gmail.com>
2026-08-14 22:21:11 +0300

    Add 'Women Clothing' to search keywords

--- update_stock.py
+++ update_stock.py
@@ -5,7 +5,7 @@ from deep_translator import GoogleTranslator
 
 # Clé API CJ (récupérée depuis les secrets GitHub)
 CJ_API_KEY = os.environ.get("CJ_API_KEY")
-MOTS_CLES_RECHERCHE = ["Lady Dress", "Women Dress", "Dress"]
+MOTS_CLES_RECHERCHE = ["Lady Dress", "Women Dress", "Dress","Women Clothing"]
 
 CJ_AUTH_URL = "https://developers.cjdropshipping.com/api2.0/v1/authentication/getAccessToken"
 CJ_PRODUCT_LIST_V2_URL = "https://developers.cjdropshipping.com/api2.0/v1/product/listV2"
