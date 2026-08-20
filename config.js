3c8aaf23821fb38bf0bc4c3b18d4931cd27d8e48
Network Access <tsikyfockso@gmail.com>
2026-08-20 14:29:56 +0300

    Create default config.js with API key and BIN ID
    Add default configuration file with API key and BIN ID.

---
+++ config.js
@@ -0,0 +1,3 @@
+// Fichier de configuration par défaut (sera écrasé par GitHub Actions lors du déploiement)
+window.CONFIG_BIN_ID = "6a86df44f5f4af5e292ca904";
+window.CONFIG_API_KEY = "$2a$10$oWpiZV8hm0i.OzlsyPjBSOjhcp7i/oia15o2pK4d7ZWNXSdE3Piva";
