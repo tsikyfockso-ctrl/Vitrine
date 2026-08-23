789ce7f93c6f7b104b692dc4bb84a281a2faa7a2
Network Access <tsikyfockso@gmail.com>
2026-08-23 13:29:06 +0300

    Update search keywords for stock update
    Removed 'Men's Clothing' from the search keywords list.

--- update_stock_homme.py
+++ update_stock_homme.py
@@ -6,7 +6,7 @@ from deep_translator import GoogleTranslator
 
 # Clé API CJ (récupérée depuis les secrets GitHub)
 CJ_API_KEY = os.environ.get("CJ_API_KEY")
-MOTS_CLES_RECHERCHE = ["Men's Clothing", "Men's Pants", "Men's Jackets", "Mens"]
+MOTS_CLES_RECHERCHE = ["Men's Pants", "Men's Jackets", "Mens"]
 
 CJ_AUTH_URL = "https://developers.cjdropshipping.com/api2.0/v1/authentication/getAccessToken"
 CJ_PRODUCT_LIST_V2_URL = "https://developers.cjdropshipping.com/api2.0/v1/product/listV2"
