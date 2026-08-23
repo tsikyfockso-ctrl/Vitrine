5d9465cbd997ba8a60c7fcf51103639732a60f98
Network Access <tsikyfockso@gmail.com>
2026-08-23 14:06:53 +0300

    Update update_stock_informatique.py

--- update_stock_informatique.py
+++ update_stock_informatique.py
@@ -6,7 +6,7 @@ from deep_translator import GoogleTranslator
 
 # Clé API CJ (récupérée depuis les secrets GitHub)
 CJ_API_KEY = os.environ.get("CJ_API_KEY")
-MOTS_CLES_RECHERCHE = ["Computer Accessories", "Keyboards", "Computer Mice", "Laptops", "USB Hubs"]
+MOTS_CLES_RECHERCHE = ["Computer's Accessories", "Keyboards", "Computer Mice", "Laptops", "USB Hubs", "computer"]
 
 CJ_AUTH_URL = "https://developers.cjdropshipping.com/api2.0/v1/authentication/getAccessToken"
 CJ_PRODUCT_LIST_V2_URL = "https://developers.cjdropshipping.com/api2.0/v1/product/listV2"
