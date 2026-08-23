1e39b5ff3f3376ab348e223d1656ae3d8b4cdcc4
Network Access <tsikyfockso@gmail.com>
2026-08-23 11:51:32 +0300

    Update search keywords for men's clothing

--- update_stock_homme.py
+++ update_stock_homme.py
@@ -6,7 +6,7 @@ from deep_translator import GoogleTranslator
 
 # Clé API CJ (récupérée depuis les secrets GitHub)
 CJ_API_KEY = os.environ.get("CJ_API_KEY")
-MOTS_CLES_RECHERCHE = ["Men Clothing", "Men Shirts", "Men Pants", "Men Jackets", "Men T-Shirts"]
+MOTS_CLES_RECHERCHE = ["Mens Clothing", "Mens Shirts", "Mens Pants", "Mens Jackets", "Mens T-Shirts", "Mens"]
 
 CJ_AUTH_URL = "https://developers.cjdropshipping.com/api2.0/v1/authentication/getAccessToken"
 CJ_PRODUCT_LIST_V2_URL = "https://developers.cjdropshipping.com/api2.0/v1/product/listV2"
