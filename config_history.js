95c7c0e95b19c28bd57cd16256dd585948f06a1f
Network Access <tsikyfockso@gmail.com>
2026-08-25 15:04:23 +0300

    Create config_history.js with payment configuration
    Add default configuration file for payment settings.

---
+++ config_history.js
@@ -0,0 +1,3 @@
+// Fichier de configuration par défaut (sera écrasé par GitHub Actions lors du déploiement)
+window.CONFIG_BIN_ID_PAYMENT = "6a8d7d06f5f4af5e293ff526";
+window.CONFIG_API_KEY_PAYMENT = "$2a$10$oWpiZV8hm0i.OzlsyPjBSOjhcp7i/oia15o2pK4d7ZWNXSdE3Piva";
