5f8601c4051a082bacad36296cf2fc108f009fac
Network Access <tsikyfockso@gmail.com>
2026-08-28 10:08:41 +0300

    Add temporary payment configuration values
    Added temporary payment configuration values.

--- config_history.js
+++ config_history.js
@@ -1,2 +1,3 @@
-window.CONFIG_BIN_ID_PAYMENT = "6a86df44f5f4af5e292ca904"; // Mettez l'ID de votre Bin de stock
-window.CONFIG_API_KEY_PAYMENT = "$2a$10$oWpiZV8hm0i.OzlsyPjBSOjhcp7i/oia15o2pK4d7ZWNXSdE3Piva";
+// Remplacez ces lignes temporairement par vos vraies valeurs entre guillemets
+const PAYMENT_BIN_ID = "6a8d7d06f5f4af5e293ff526"; // Votre ID de Bin de paiement
+const PAYMENT_API_KEY = "$2a$10$oWpiZV8hm0i.OzlsyPjBSOjhcp7i/oia15o2pK4d7ZWNXSdE3Piva"; // Votre clé API JSONBin
