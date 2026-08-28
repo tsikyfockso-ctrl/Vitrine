fee1e1f76fe20befc707a6ff7c9ccc3ad007f5c8
Network Access <tsikyfockso@gmail.com>
2026-08-28 13:27:31 +0300

    Delete config_history.js

--- config_history.js
+++
@@ -1,3 +0,0 @@
-// Remplacez ces lignes temporairement par vos vraies valeurs entre guillemets
-const PAYMENT_BIN_ID = "6a8d7d06f5f4af5e293ff526"; // Votre ID de Bin de paiement
-const PAYMENT_API_KEY = "$2a$10$oWpiZV8hm0i.OzlsyPjBSOjhcp7i/oia15o2pK4d7ZWNXSdE3Piva"; // Votre clé API JSONBin
