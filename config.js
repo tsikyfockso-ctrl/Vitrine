eb957b57278e438d2058a036dc55eb06d5e0f05d
Network Access <tsikyfockso@gmail.com>
2026-08-31 14:12:15 +0300

    Add configuration for NPoint.io API endpoints

---
+++ config.js
@@ -0,0 +1,8 @@
+// ==========================================
+// CONFIGURATION DES BINS NPOINT.IO
+// ==========================================
+
+const CONFIG = {
+    MESSAGES_URL: "https://api.npoint.io/7bb351d44d168f9e4bea",
+    PAIEMENTS_URL: "https://api.npoint.io/232b99bcbfdc6b24b18e"
+};
