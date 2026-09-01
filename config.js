625984d5515b0543e548d2b074535054b613247c
Network Access <tsikyfockso@gmail.com>
2026-09-01 13:44:03 +0300

    Update API URLs in config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 // ==========================================
 
 const CONFIG = {
-    MESSAGES_URL: "https://api.npoint.io/7bb351d44d168f9e4bea",
-    PAIEMENTS_URL: "https://api.npoint.io/232b99bcbfdc6b24b18e"
+    MESSAGES_URL: "https://api.npoint.io/7bb351d44d168f9e4bea/messages",
+    PAIEMENTS_URL: "https://api.npoint.io/232b99bcbfdc6b24b18e/paiements"
 };
