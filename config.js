449ccca749d705e2f5ec10d6cd2e855ba9aa3d77
Network Access <tsikyfockso@gmail.com>
2026-09-01 15:34:34 +0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// ==========================================
-// CONFIGURATION DES BINS NPOINT.IO
-// ==========================================
-
-const CONFIG = {
-    MESSAGES_URL: "https://api.npoint.io/7bb351d44d168f9e4bea/messages",
-    PAIEMENTS_URL: "https://api.npoint.io/232b99bcbfdc6b24b18e/paiements"
-};
